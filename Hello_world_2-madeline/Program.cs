dc289cc7c8fc9679213961d3f39b0b23fed9a862
madeline allison restin <144450180+maddycode15@users.noreply.github.com>
2023-09-12 15:05:59 -0300

    madeline-hello world 2

---
+++ Hello_world_2-madeline/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace maddy_allison_hello_world
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hello world! Uwu");
+            Console.ReadKey(); // wait for a user key press
+
+
+        } /// end of the program
+    }
+}
---
+++ hello world 2 madeline.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace maddy_allison_hello_world_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hello world! Uwu its day 2 of programming");
+            Console.ReadKey(); // wait for a user key press
+
+
+        } /// end of the program
+    }
+}
